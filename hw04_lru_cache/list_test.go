73a5b3bacb521d8410efc419a2d21216f203183e
Aleksey <aleksey.yurev@quadcode.com>
2022-08-11 20:31:00 +0300

    HW4 is completed

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -15,37 +15,94 @@ func TestList(t *testing.T) {
 		require.Nil(t, l.Back())
 	})
 
-	t.Run("complex", func(t *testing.T) {
+	t.Run("simple push front", func(t *testing.T) {
 		l := NewList()
 
-		l.PushFront(10) // [10]
-		l.PushBack(20)  // [10, 20]
-		l.PushBack(30)  // [10, 20, 30]
-		require.Equal(t, 3, l.Len())
+		l.PushFront(10)
 
-		middle := l.Front().Next // 20
-		l.Remove(middle)         // [10, 30]
+		require.Equal(t, 1, l.Len())
+		front := l.Front()
+		back := l.Back()
+		require.Equal(t, 10, front.Value)
+		require.Equal(t, 10, back.Value)
+		require.Nil(t, front.Prev)
+		require.Nil(t, front.Next)
+		require.Nil(t, back.Prev)
+		require.Nil(t, back.Next)
+	})
+
+	t.Run("simple push back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+
+		require.Equal(t, 1, l.Len())
+		front := l.Front()
+		back := l.Back()
+		require.Equal(t, 10, front.Value)
+		require.Equal(t, 10, back.Value)
+		require.Nil(t, front.Prev)
+		require.Nil(t, front.Next)
+		require.Nil(t, back.Prev)
+		require.Nil(t, back.Next)
+	})
+
+	t.Run("simple remove", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+		l.PushBack(30)
+
+		middle := l.Front().Next
+		l.Remove(middle)
+
 		require.Equal(t, 2, l.Len())
+		front := l.Front()
+		back := l.Back()
+		require.Equal(t, 10, front.Value)
+		require.Equal(t, 30, front.Next.Value)
+		require.Equal(t, 30, back.Value)
+		require.Equal(t, 10, back.Prev.Value)
 
-		for i, v := range [...]int{40, 50, 60, 70, 80} {
-			if i%2 == 0 {
-				l.PushFront(v)
-			} else {
-				l.PushBack(v)
-			}
-		} // [80, 60, 40, 10, 30, 50, 70]
+		l.Remove(back)
+		require.Equal(t, 1, l.Len())
+		front = l.Front()
+		require.Equal(t, 10, front.Value)
+		require.Nil(t, front.Prev)
+		require.Nil(t, front.Next)
 
-		require.Equal(t, 7, l.Len())
-		require.Equal(t, 80, l.Front().Value)
-		require.Equal(t, 70, l.Back().Value)
+		l.Remove(front)
+		require.Equal(t, 0, l.Len())
+		front = l.Front()
+		require.Nil(t, front)
+	})
 
-		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
-		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
+	t.Run("simple remove", func(t *testing.T) {
+		l := NewList()
 
-		elems := make([]int, 0, l.Len())
-		for i := l.Front(); i != nil; i = i.Next {
-			elems = append(elems, i.Value.(int))
-		}
-		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+		l.PushBack(10) // [10]
+		l.PushBack(20) // [10,20]
+		l.PushBack(30) // [10,20,30]
+
+		middle := l.Front().Next
+		l.MoveToFront(middle) // [20,10,30]
+
+		require.Equal(t, 3, l.Len())
+		front := l.Front()
+		back := l.Back()
+		require.Equal(t, 20, front.Value)
+		require.Equal(t, 10, front.Next.Value)
+		require.Equal(t, 30, back.Value)
+		require.Equal(t, 10, back.Prev.Value)
+
+		l.MoveToFront(back) // [30,20,10]
+		require.Equal(t, 3, l.Len())
+		front = l.Front()
+		back = l.Back()
+		require.Equal(t, 30, front.Value)
+		require.Equal(t, 20, front.Next.Value)
+		require.Equal(t, 10, back.Value)
+		require.Equal(t, 20, back.Prev.Value)
 	})
 }
