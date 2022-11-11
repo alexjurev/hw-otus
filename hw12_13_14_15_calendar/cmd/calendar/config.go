6eb5b5990563d88bab89d9ee31223494ac724144
Aleksey Yurev <al.yuryev@yandex.ru>
2022-11-11 17:27:41 +0300

    HW12 is completed

--- hw12_13_14_15_calendar/cmd/calendar/config.go
+++ hw12_13_14_15_calendar/cmd/calendar/config.go
@@ -1,20 +1,51 @@
 package main
 
-// При желании конфигурацию можно вынести в internal/config.
-// Организация конфига в main принуждает нас сужать API компонентов, использовать
-// при их конструировании только необходимые параметры, а также уменьшает вероятность циклической зависимости.
+import (
+	"fmt"
+	"strings"
+
+	"github.com/alexjurev/hw-otus/hw12_13_14_15_calendar/internal/logger"
+	internalhttp "github.com/alexjurev/hw-otus/hw12_13_14_15_calendar/internal/server/http"
+	"github.com/alexjurev/hw-otus/hw12_13_14_15_calendar/internal/storagebuilder"
+	"github.com/spf13/viper"
+)
+
+const envConfigPrefix = "$env:"
+
 type Config struct {
-	Logger LoggerConf
-	// TODO
+	Server  internalhttp.Config
+	Logger  logger.Config
+	Storage storagebuilder.Config
 }
 
-type LoggerConf struct {
-	Level string
-	// TODO
-}
+func NewConfig(configFile string) (Config, error) {
+	config := Config{}
+	viper.SetConfigFile(configFile)
 
-func NewConfig() Config {
-	return Config{}
-}
+	viper.SetDefault("server.host", "127.0.0.1")
+	viper.SetDefault("server.port", "8080")
+	viper.SetDefault("logger.level", "WARN")
+	viper.SetDefault("storage.storageType", "memory")
 
-// TODO
+	err := viper.ReadInConfig()
+	if err != nil {
+		return config, fmt.Errorf("reading config %q: %w", configFile, err)
+	}
+	keys := viper.AllKeys()
+	for _, key := range keys {
+		env := viper.GetString(key)
+		if strings.HasPrefix(env, envConfigPrefix) {
+			err := viper.BindEnv(key, env[len(envConfigPrefix):])
+			if err != nil {
+				return Config{}, fmt.Errorf("preparing config: %w", err)
+			}
+		}
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return config, fmt.Errorf("decoding into config struct: %w", err)
+	}
+
+	return config, nil
+}
--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -1,3 +1,11 @@
-module github.com/fixme_my_friend/hw12_13_14_15_calendar
+module github.com/alexjurev/hw-otus/hw12_13_14_15_calendar
 
 go 1.16
+
+require (
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/lib/pq v1.10.4
+	github.com/sirupsen/logrus v1.6.0
+	github.com/spf13/viper v1.14.0
+	github.com/stretchr/testify v1.8.1
+)
